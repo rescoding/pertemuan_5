d5245d4b171fad22542aa8723c45144f89722669
rescoding <apriliadalapan03@gmail.com>
2023-03-30 16:42:49 +0800

    pertemuan6

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,62 +2,93 @@ function Card() {
     return (
         <>
             <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-blue-500 to-orange-600">
-                <div class="w-1/2 bg-white rounded-lg shadow-2xl p-8 m-4">
-                    <h1 class="block w-full tect-center tect-gray-800 text-2xl font-bold  mb-6">
-                        Kartu Tanda Penduduk
+                <div class="w-1/2 h-4/2 bg-white bg-opacity-50 rounded-lg shadow-2xl p-8 m-4">
+                    <h1 class="block w-full text-center text-gray-800 text-2xl font-bold  mb-6">
+                        Register
                     </h1>
                     <form action="/" method="post">
-                        <div class="flex flex-col mt-8 mb-4">
-                        <img class="rounded w-40 h-50" src="April.jpg"/>
+
+                        <div class="flex flex-col mb-4">
                             <label
-                                class="mb-2 font-bold text-lg text-gray-900"
-                                for="Nama"
+                            class="mb-2 font-bold text-lg text-gray-900"
+                            for="nama"
                             >
                                 Nama
                             </label>
                             <input
-                                class="border py-2 px-3 text-grey-800"
-                                type="text"
+                            class="border py-1 px-3 text-grey-800"
+                            type="text" placeholder="Aprilia Anglika Dalapan"
                                 name="nama"
                                 id="nama"
-                                placeholder="Aprilia Angelika Dalapan"
                             />
                         </div>
                         <div class="flex flex-col mb-4">
                             <label
-                                class="mb-2 font-bold text-lg text-gray-900"
-                                for="Alamat"
+                            class="mb-2 font-bold text-lg text-gray-900"
+                            for="gender"
                             >
-                                Alamat
+                                Gender
                             </label>
+                            <div>
                             <input
-                                class="border py-2 px-3 text-grey-800"
-                                type="text"
-                                name="Alamat"
-                                id="Alamat"
-                                placeholder="Kampung Talengen"
+                            class="border py-2 text-grey-800"
+                            type="radio"
+                            name="gender"
+                            id="pria"
                             />
+
+                            <label
+                                class="mb-2 text-gray-900"
+                                for="pria"
+                            >
+                                Pria
+                            </label>
+                            </div>
+                        </div>
+                        <div>
+                            <input
+                                class="border py-2 text-grey-800"
+                                type="radio"
+                                name="gender"
+                                id="wanita"
+                            />
+                            <label
+                            class="mb-2 text-gray-900"
+                            for="wanita"
+                            >Wanita</label>
                         </div>
                         <div class="flex flex-col mb-4">
                             <label
-                                class="mb-2 font-bold text-lg text-gray-900"
-                                for="Agama"
-                            >
-                                Agama
-                            </label>
-                            <input
-                                class="border py-2 px-3 text-grey-800"
-                                type="text"
-                                name="Agama"
-                                id="Agama"
-                                placeholder="Kristen"
-                            />
-                        </div>
-                        </form>
-                        <a
-                        class="block w-full text-center no-underline mt-4 text-sm text-gray-700 hover:text-gry-900"
+                            class="mb-2 font-bold text-lg text-gray-900"
+                            for="agama"
+                        >
+                            Agama
+                        </label>
+                        <select
+                            class="border py-1 px-3 text-gray-800"
+                            name="agama"
+                            id="agama"
+                        >
+                            <option value="Protestan">Kristen Protestan</option>
+                            <option value="Katholik">Katholik</option>
+                            <option value="Islam">Islam</option>
+                            <option value="Hindu">Hindu</option>
+                            <option value="Budha">Budha</option>
+                            <option value="Kepercayaan">Aliran Kepercayaan</option>
+                            </select>
+                    </div>
+                    <button
+                    class="block bg-blue-600 hover:bg-blue-00 text-white uppercase texty-lg mx-auto p-4 rounded"
+                    type="submit"
+                    >
+                        Buat Akun
+                    </button>
+                </form>
+                <a
+                        class="block w-full text-center no-underline mt-4 text-sm text-gray-700 hover:texty-gray-900"
                         href="/"
                     >
+                        Telah Memiliki Akun?
                     </a>
                 </div>
             </div>
